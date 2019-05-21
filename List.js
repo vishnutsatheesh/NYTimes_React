7397eaa6586de21d3e4d5990da2532c66efbd5de
Vishnu T Satheesh <vishnutsatheesh@gmail.com>
2019-05-21 16:30:35 +0530

    Api integration with Flat list integration

--- List.js
+++ List.js
@@ -1,41 +1,80 @@
 import React, { Component } from 'react'
-import { Text, View, TouchableOpacity, StyleSheet,AppRegistry, Image,ActivityIndicator  } from 'react-native'
+import { Text, View, TouchableOpacity, StyleSheet,AppRegistry, Image,ActivityIndicator,FlatList  } from 'react-native'
 
 class List extends Component {
 
-   state = {
-      names: [
-         {
-            id: 0,
-            name: 'Ben'
-         },
-         {
-            id: 1,
-            name: 'Susan',
-         },
-         {
-            id: 2,
-            name: 'Robert',
-         },
-         {
-            id: 3,
-            name: 'Mary',
-         }
-      ]
-   }
+
+
+
+
    alertItemName = (item) => {
       alert(item.name)
    }
 
+   constructor(props) {
+       super(props);
 
+       this.state = {
+         loading: true,
+         data: [],
+         page: 1,
+         seed: 1,
+         error: null,
+         refreshing: false,
+         states : {
+            names: [
+               {
+                  id: 0,
+                  name: 'Ben'
+               },
+               {
+                  id: 1,
+                  name: 'Susan',
+               },
+               {
+                  id: 2,
+                  name: 'Robert',
+               },
+               {
+                  id: 3,
+                  name: 'Mary',
+               }
+            ]
+         },
+       };
+     }
    componentDidMount(){
-       return fetch('https://facebook.github.io/react-native/movies.json')
+       return fetch('http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/7.json?api-key=54acea37ea804f3eafc2404736bafca3')
          .then((response) => response.json())
          .then((responseJson) => {
            this.setState({
-             isLoading: false,
-             dataSource: responseJson.movies,
+             loading: false,
+             dataSource: responseJson.results,
+             states : {
+                names: [
+                   {
+                      id: 0,
+                      name: 'Ben'
+                   },
+                   {
+                      id: 1,
+                      name: 'Susanccccccccc',
+                   },
+                   {
+                      id: 2,
+                      name: 'Robertccccc',
+                   },
+                   {
+                      id: 3,
+                      name: 'Marddwy',
+                   }
+                ]
+             },
+
            }, function(){
+
+
+            // this.setview();
 
            });
 
@@ -48,55 +87,68 @@ class List extends Component {
 
 
    render() {
+
+     if (this.state.loading) {
+     return (
+       <View style={{flex: 1, paddingTop: 20, backgroundColor: "red"}}>
+         <ActivityIndicator />
+       </View>
+     );
+   }
+
       return (
          <View>
-            {
-               this.state.names.map((item, index) => (
-                  <TouchableOpacity
-                     key = {item.id}
-                      style = {styles.container}
-                     onPress = {() => this.alertItemName(item)}>
+
+         <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) =>
+
+          <TouchableOpacity
+             key = {item.id}
+              style = {styles.container}
+             onPress = {() => this.alertItemName(item.jsonObj["media-metadata"])}>
+             <View style={{flex: 1, flexDirection: 'row'}}>
+
+  <View style={{ width: 100,height: 100, backgroundColor: 'powderblue'}} >
+  <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg", isStatic: true}} style={{width: 100, height: 100}}/>
+
+  </View>
+
+  <View style={{flex: 1,height: 100, flexDirection: 'column'}}>
+
+  <View style={{ flex: 1, backgroundColor: 'skyblue'}} >
+  <Text style = {styles.wordBold}>{item.title}</Text>
+  </View>
+
+  <View style={{ flex: 1, backgroundColor: 'steelblue'}} >
+  <Text style = {styles.text}>{item.byline}</Text>
+  </View>
+
+  <View style={{flex: 1, flexDirection: 'row'}}>
+
+  <View style={{ flex: 1, backgroundColor: 'green'}} >
+  <Text style = {styles.text}>{item.source}</Text>
+  </View>
+
+  <View style={{ flex: 1, backgroundColor: 'yellow'}} >
+  <Text style = {styles.text}>{item.published_date}</Text>
+  </View>
+
+
+  </View>
+
+  </View>
+
+  </View>
+
+
+          </TouchableOpacity>
+
+        }
+        />
 
 
 
-                     <View style={{flex: 1, flexDirection: 'row'}}>
-
-        <View style={{ width: 100,height: 100, backgroundColor: 'powderblue'}} >
-        <Image source={{uri: "https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg", isStatic: true}} style={{width: 100, height: 100}}/>
-
-        </View>
-
-         <View style={{flex: 1,height: 100, flexDirection: 'column'}}>
-
-         <View style={{ flex: 1, backgroundColor: 'skyblue'}} >
-         <Text style = {styles.wordBold}>{item.name}</Text>
-         </View>
-
-         <View style={{ flex: 1, backgroundColor: 'steelblue'}} >
-         <Text style = {styles.text}>{item.name}</Text>
-         </View>
-
-          <View style={{flex: 1, flexDirection: 'row'}}>
-
-          <View style={{ flex: 1, backgroundColor: 'green'}} >
-          <Text style = {styles.text}>{item.name}</Text>
-          </View>
-
-          <View style={{ flex: 1, backgroundColor: 'yellow'}} >
-          <Text style = {styles.text}>{item.name}</Text>
-          </View>
-
-
-          </View>
-
-         </View>
-
-      </View>
-
-
-                  </TouchableOpacity>
-               ))
-            }
          </View>
       )
    }
