be371418df263d0e77a1c7f788a88569e3d6b9c6
vishnu t satheesh <vishnutsatheesh@gmail.com>
2019-05-30 22:45:53 +0530

    no major chnages done

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import List from './List.js'
+import {createStackNavigator, createAppContainer} from 'react-navigation';
 
 const App = () => {
    return (
       <List />
    )
 }
+
+const MainNavigator = createStackNavigator({
+  Home: {screen: HomeScreen},
+  Profile: {screen: ProfileScreen},
+});
+
+const App = createAppContainer(MainNavigator);
+const App = createAppContainer(MainNavigator);
+const App = createAppContainer(MainNavigator);
+const App = createAppContainer(MainNavigator);
+const App = createAppContainer(MainNavigator);
+const App = createAppContainer(MainNavigator);
+
+
 export default App
+
